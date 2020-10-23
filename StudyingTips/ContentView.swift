89b4cf918737712e26dcbccf63e9dcfb59865cdd
Austin Felder <afellafelder@yahoo.com>
2020-10-23 04:53:56 -0400

    Initial Commit

---
+++ StudyingTips/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  StudyingTips
+//
+//  Created by Austin Felder on 10/23/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ StudyingTips/StudyingTipsApp.swift
@@ -0,0 +1,17 @@
+//
+//  StudyingTipsApp.swift
+//  StudyingTips
+//
+//  Created by Austin Felder on 10/23/20.
+//
+
+import SwiftUI
+
+@main
+struct StudyingTipsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
